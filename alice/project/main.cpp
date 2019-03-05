ca1d2d2629f24ea755c8b5b8c894a8e3a512d948
Alice (KluchnikovAK) <alice@example.com>
2019-03-05 16:36:31 +0300

    MAX

--- alice/project/main.cpp
+++ alice/project/main.cpp
@@ -5,10 +5,19 @@ using namespace std;
 int main()
 {
     cout<<" Enter A and B:";
-    int a,b;
+    int a,b,max;
     cin>>a>>b;
-cout << "A + B = " << a + b << '\n'
+    cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n';
+    if(a+b>a-b && a+b>a*b && a+b>a/b)
+        max=a+b;
+    if(a-b>a+b && a-b>a*b && a-b>a/b)
+        max=a-b;
+    if(a*b>a-b && a*b>a+b && a*b>a/b)
+        max=a*b;
+    if(a/b>a-b && a/b>a*b && a/b>a+b)
+        max=a/b;
+    cout<<"MAX"<<max<<'\n';
 }
